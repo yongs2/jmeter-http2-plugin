2a67a625d529d94769d4c4b6d34377bec493b6bf
yongs2 <yongs2@hotmail.com>
2020-06-01 10:16:35 +0900

    add PUT method

--- src/main/java/com/blazemeter/jmeter/http2/sampler/RequestBody.java
+++ src/main/java/com/blazemeter/jmeter/http2/sampler/RequestBody.java
@@ -36,6 +36,7 @@ public class RequestBody {
     private static String buildPostBody(String method, String contentEncoding, Arguments args, boolean sendParamsAsBody)
             throws UnsupportedEncodingException {
         if ((HTTPConstants.POST.equals(method) 
+            || HTTPConstants.PUT.equals(method)
             || HTTPConstants.PATCH.equals(method) )
             && !sendParamsAsBody) {
             PropertyIterator iter = args.getArguments().iterator();
